557b9e8074d42ab62fd7c73a1ad498dd8f3202d6
Antonio <antonio.campos108@alu.uhu.es>
2024-09-24 11:25:18 +0200

    Añadido otro mensaje para pasar de la fase 6 a 7

--- src/main/tutorial.java
+++ src/main/tutorial.java
@@ -5,5 +5,6 @@ public class tutorial {
 	public static void main(String[] args) {
 		System.out.println("Hola CMEPPS!");
 		System.out.println("Cambio de Rama 2 a 3");
+		System.out.println("Cambio de fase 6 a 7");
 	}
 }
